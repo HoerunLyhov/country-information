7fa14900b744531c701160e0e3dd7fd766d138d5
Hoeurn Lyhov <hoeurnlyhov16@kit.edu.kh>
2023-08-17 18:22:52 +0700

    Add BootStrap5 to Table

--- resources/views/country.blade.php
+++ resources/views/country.blade.php
@@ -12,10 +12,14 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css"/>
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
+    <!-- BootStrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 
   </header>
-  <body class="antialiased">
-    <table id="table-id">
+  <body class="antialiased mx-4 mb-4 mt-5">
+    <div class="d-flex justify-content-center mb-4"><h1>Country Information</h1></div>
+    <table id="table-id" class="table table-hover display">
       <thead>
         <tr>
           <th>Flags</th>
